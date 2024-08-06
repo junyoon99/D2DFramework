851b125c8b2e3d0b0a10d9f2a033378ad2cd183c
Hyeontae An <83248120+bearbig-12@users.noreply.github.com>
2024-08-06 16:55:37 +0900

    초기 커밋.

---
+++ winmain.cpp
@@ -0,0 +1,31 @@
+#include <Windows.h>
+#include "D2DFramework.h"
+
+
+
+D2DFramework myFramework;
+
+int WINAPI WinMain(
+	_In_ HINSTANCE hInstance,
+	_In_opt_ HINSTANCE hPrevInstance,
+	_In_ LPSTR lpCmdLine,
+	_In_ int nShowCmd
+) 
+{
+	int ret{};
+	try 
+	{
+		myFramework.Init(hInstance);
+
+		ret = myFramework.GameLoop();
+
+		// 4. «ÿ¡¶
+		myFramework.Release();
+	}
+	catch (const com_exception& e) 
+	{
+		OutputDebugStringA(e.what());
+	}
+
+	return ret;
+}
